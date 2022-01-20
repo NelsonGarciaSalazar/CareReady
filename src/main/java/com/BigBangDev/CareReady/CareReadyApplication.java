277633bd240dfb2f255ad46693b365068afbcb2c
Nelson Garcia <nelsong.salazar@gmail.com>
2022-01-20 16:32:50 -0500

    First Commit: Create project with: Spring MVC 2.6.3 Java 11 Change port = 8085 Change Path = /app

---
+++ src/main/java/com/BigBangDev/CareReady/CareReadyApplication.java
@@ -0,0 +1,13 @@
+package com.BigBangDev.CareReady;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareReadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareReadyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BigBangDev/CareReady/CareReadyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BigBangDev.CareReady;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareReadyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
